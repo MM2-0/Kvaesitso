6a738a4bbdfa85ca5b4fe2e6ee412b023c35a62b
MM20 <15646950+MM2-0@users.noreply.github.com>
2021-09-25 20:20:06 +0200

    Add back button to settings screen

--- ui/src/main/java/de/mm20/launcher2/ui/screens/settings/SettingsMainScreen.kt
+++ ui/src/main/java/de/mm20/launcher2/ui/screens/settings/SettingsMainScreen.kt
@@ -1,22 +1,31 @@
 package de.mm20.launcher2.ui.screens.settings
 
-import androidx.compose.material.Scaffold
-import androidx.compose.material.Text
-import androidx.compose.material.TopAppBar
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.ArrowBack
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import com.google.accompanist.insets.statusBarsPadding
 import de.mm20.launcher2.ui.R
+import de.mm20.launcher2.ui.locals.LocalNavController
 
 @Composable
 fun SettingsMainScreen() {
+    val navController = LocalNavController.current
     Scaffold(topBar = {
         TopAppBar(
             title = {
                 Text(stringResource(id = R.string.title_activity_settings))
             },
-            modifier = Modifier.statusBarsPadding()
+            modifier = Modifier.statusBarsPadding(),
+            navigationIcon = {
+                IconButton(onClick = {
+                    navController?.navigateUp()
+                }) {
+                    Icon(imageVector = Icons.Rounded.ArrowBack, contentDescription = "Back")
+                }
+            }
         )
     }) {
     }
