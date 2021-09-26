1ed359f6cf4300c300e64d7e644bd85ced3ad0a0
MM20 <15646950+MM2-0@users.noreply.github.com>
2021-09-26 16:46:14 +0200

    Expose PreferenceCategory column scope

--- ui/src/main/java/de/mm20/launcher2/ui/component/preferences/PreferenceCategory.kt
+++ ui/src/main/java/de/mm20/launcher2/ui/component/preferences/PreferenceCategory.kt
@@ -1,9 +1,6 @@
 package de.mm20.launcher2.ui.component.preferences
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
@@ -15,7 +12,7 @@ import androidx.compose.ui.unit.dp
 @Composable
 fun PreferenceCategory(
     title: String? = null,
-    content: @Composable () -> Unit
+    content: @Composable ColumnScope.() -> Unit
 ) {
     Surface(
         elevation = 2.dp,
