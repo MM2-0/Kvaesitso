1dca528bc401696e836a94a7fce37c6f8307ff75
MM20 <15646950+MM2-0@users.noreply.github.com>
2021-09-26 22:16:50 +0200

    Tweak wallpaper color scheme

--- ui/src/main/java/de/mm20/launcher2/ui/theme/colors/WallpaperColorScheme.kt
+++ ui/src/main/java/de/mm20/launcher2/ui/theme/colors/WallpaperColorScheme.kt
@@ -1,15 +1,15 @@
 package de.mm20.launcher2.ui.theme.colors
 
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.compositeOver
 import de.mm20.launcher2.ui.theme.WallpaperColors
 
 class WallpaperColorScheme(
     val wallpaperColors: WallpaperColors
-): ColorScheme() {
+) : ColorScheme() {
     override val neutral1 = colorSwatch(wallpaperColors.primary)
     override val neutral2 = colorSwatch(wallpaperColors.primary)
-    override val accent1 = colorSwatch(wallpaperColors.primary)
+    override val accent1 = colorSwatch(
+        wallpaperColors.tertiary ?: wallpaperColors.secondary ?: wallpaperColors.primary
+    )
     override val accent2 = colorSwatch(wallpaperColors.secondary ?: wallpaperColors.primary)
-    override val accent3 = colorSwatch(wallpaperColors.tertiary ?: wallpaperColors.primary)
+    override val accent3 = colorSwatch(wallpaperColors.primary)
 }
