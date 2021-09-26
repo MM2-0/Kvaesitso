ac283f99b374290757bb1d9abee8455d86eb85a0
MM20 <15646950+MM2-0@users.noreply.github.com>
2021-09-26 22:57:58 +0200

    Add SwitchPreference

---
+++ ui/src/main/java/de/mm20/launcher2/ui/component/preferences/SwitchPreference.kt
@@ -0,0 +1,28 @@
+package de.mm20.launcher2.ui.component.preferences
+
+import androidx.compose.material.Switch
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.vector.ImageVector
+
+@Composable
+fun SwitchPreference(
+    title: String,
+    icon: ImageVector? = null,
+    summary: String? = null,
+    value: Boolean,
+    onValueChanged: (Boolean) -> Unit,
+    enabled: Boolean = true
+) {
+    Preference(
+        title = title,
+        icon = icon,
+        summary = summary,
+        enabled = enabled,
+        onClick = {
+            onValueChanged(!value)
+        },
+        controls = {
+            Switch(checked = value, onCheckedChange = onValueChanged)
+        }
+    )
+}
