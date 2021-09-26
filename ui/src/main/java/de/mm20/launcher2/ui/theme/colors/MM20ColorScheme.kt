de13ea8826241afce04a3f7b2b1ad1592100c3b2
MM20 <15646950+MM2-0@users.noreply.github.com>
2021-09-26 22:09:07 +0200

    Add MM20 color scheme

---
+++ ui/src/main/java/de/mm20/launcher2/ui/theme/colors/MM20ColorScheme.kt
@@ -0,0 +1,11 @@
+package de.mm20.launcher2.ui.theme.colors
+
+import androidx.compose.ui.graphics.Color
+
+class MM20ColorScheme: ColorScheme() {
+    override val neutral1 = colorSwatch(Color(0xff233139))
+    override val neutral2 = colorSwatch(Color(0xff233139))
+    override val accent1 = colorSwatch(Color(0xfface330))
+    override val accent2 = colorSwatch(Color(0xff496777))
+    override val accent3 = colorSwatch(Color(0xfface330))
+}
