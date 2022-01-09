eaa1848f9731f4e900f5356d1a05a98b3d2d03f4
MM20 <15646950+MM2-0@users.noreply.github.com>
2022-01-09 14:59:30 +0100

    LicenseScreen: load license text in worker thread

--- ui/src/main/java/de/mm20/launcher2/ui/settings/license/LicenseScreenVM.kt
+++ ui/src/main/java/de/mm20/launcher2/ui/settings/license/LicenseScreenVM.kt
@@ -4,11 +4,15 @@ import android.app.Application
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.liveData
 import de.mm20.launcher2.licenses.OpenSourceLibrary
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 
 class LicenseScreenVM(private val context: Application) : AndroidViewModel(context) {
     fun getLicenseText(library: OpenSourceLibrary) = liveData<String?> {
-        val text = context.resources.openRawResource(library.licenseText).reader()
-            .readText()
+        val text = withContext(Dispatchers.IO) {
+            context.resources.openRawResource(library.licenseText).reader()
+                .readText()
+        }
         emit(text)
     }
 }
