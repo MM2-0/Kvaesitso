ef01439ded3b81d39ab9ee361dd5d2547df3b408
MM20 <15646950+MM2-0@users.noreply.github.com>
2022-01-09 01:14:36 +0100

    Add export log to debug settings screen

--- ui/src/main/java/de/mm20/launcher2/ui/settings/debug/DebugSettingsScreen.kt
+++ ui/src/main/java/de/mm20/launcher2/ui/settings/debug/DebugSettingsScreen.kt
@@ -1,16 +1,24 @@
 package de.mm20.launcher2.ui.settings.debug
 
+import android.content.Intent
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
+import androidx.core.content.FileProvider
 import de.mm20.launcher2.crashreporter.CrashReporter
+import de.mm20.launcher2.debug.DebugInformationDumper
+import de.mm20.launcher2.ktx.tryStartActivity
 import de.mm20.launcher2.ui.R
 import de.mm20.launcher2.ui.component.preferences.Preference
 import de.mm20.launcher2.ui.component.preferences.PreferenceScreen
+import kotlinx.coroutines.launch
+import java.io.File
 
 @Composable
 fun DebugSettingsScreen() {
     val context = LocalContext.current
+    val scope = rememberCoroutineScope()
     PreferenceScreen(
         stringResource(R.string.preference_screen_debug)
     ) {
@@ -20,6 +28,28 @@ fun DebugSettingsScreen() {
                 summary = stringResource(R.string.preference_crash_reporter_summary),
                 onClick = {
                     context.startActivity(CrashReporter.getLaunchIntent())
+                })
+
+            Preference(
+                title = stringResource(R.string.preference_export_log),
+                onClick = {
+                    scope.launch {
+                        val path = DebugInformationDumper().dump(context)
+                        context.tryStartActivity(
+                            Intent.createChooser(
+                                Intent(Intent.ACTION_SEND).apply {
+                                    type = "text/plain"
+                                    putExtra(
+                                        Intent.EXTRA_STREAM, FileProvider.getUriForFile(
+                                            context,
+                                            context.applicationContext.packageName + ".fileprovider",
+                                            File(path)
+                                        )
+                                    )
+                                }, null
+                            )
+                        )
+                    }
                 })
         }
     }
