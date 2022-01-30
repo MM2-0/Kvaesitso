e19fef9b18213f32f776e6c6518f8252f0702c9b
MM20 <15646950+MM2-0@users.noreply.github.com>
2022-01-30 14:12:54 +0100

    v1.4.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,7 +26,7 @@ android {
         minSdk = sdk.versions.minSdk.get().toInt()
         targetSdk = sdk.versions.targetSdk.get().toInt()
         versionCode = versionCodeDate()
-        versionName = "1.3.0"
+        versionName = "1.4.0"
         multiDexEnabled = true
         signingConfig = signingConfigs.getByName("debug")
     }
