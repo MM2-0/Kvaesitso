658a29427c5ea5d191715d7d32dfc1a37ed3b64a
MM20 <15646950+MM2-0@users.noreply.github.com>
2022-02-07 18:36:56 +0100

    Version 1.4.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,7 +26,7 @@ android {
         minSdk = sdk.versions.minSdk.get().toInt()
         targetSdk = sdk.versions.targetSdk.get().toInt()
         versionCode = versionCodeDate()
-        versionName = "1.4.2"
+        versionName = "1.4.3"
         multiDexEnabled = true
         signingConfig = signingConfigs.getByName("debug")
     }
