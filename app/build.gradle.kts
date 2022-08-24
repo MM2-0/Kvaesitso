0e053732222897e2298f415d745a8c377fa0d442
MM20 <15646950+MM2-0@users.noreply.github.com>
2022-08-24 16:50:16 +0200

    Version 1.14.5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,7 +26,7 @@ android {
         minSdk = sdk.versions.minSdk.get().toInt()
         targetSdk = sdk.versions.targetSdk.get().toInt()
         versionCode = versionCodeDate()
-        versionName = "1.14.4"
+        versionName = "1.14.5"
         signingConfig = signingConfigs.getByName("debug")
     }
     buildTypes {
@@ -73,7 +73,7 @@ fun buildTime(): String {
 }
 
 fun versionCodeDate(): Int {
-    val df = SimpleDateFormat("yyyyMMdd01")
+    val df = SimpleDateFormat("yyyyMMdd00")
     return df.format(Date()).toInt()
 }
 
