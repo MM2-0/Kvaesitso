2d6079783025f9d6ec1c4c4ea76cdd69ea89016e
MM20 <15646950+MM2-0@users.noreply.github.com>
2022-09-03 16:18:49 +0200

    Add assistant activity

---
+++ ui/src/main/java/de/mm20/launcher2/ui/assistant/AssistantActivity.kt
@@ -0,0 +1,5 @@
+package de.mm20.launcher2.ui.assistant
+
+import de.mm20.launcher2.ui.launcher.SharedLauncherActivity
+
+class AssistantActivity: SharedLauncherActivity(LauncherActivityMode.Assistant)
