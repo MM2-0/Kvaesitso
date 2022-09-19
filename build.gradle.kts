ceb3b565076dc2ec72949791a909f2a2b22643e3
MM20 <15646950+MM2-0@users.noreply.github.com>
2022-09-19 22:00:43 +0200

    Upgradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:7.2.2")
+        classpath("com.android.tools.build:gradle:7.3.0")
         classpath(libs.kotlin.gradle)
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.10")
         // NOTE: Do not place your application dependencies here; they belong
