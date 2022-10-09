19aa3da68bac480dfbdbba289fee5c12a5899180
MM20 <15646950+MM2-0@users.noreply.github.com>
2022-10-09 23:05:18 +0200

    Docs: change screenshot size and spacing

--- docs/src/components/HomepageScreenshots/index.tsx
+++ docs/src/components/HomepageScreenshots/index.tsx
@@ -8,24 +8,36 @@ export default function HomepageScreenshots(): JSX.Element {
       <div className="container padding-vert--xl">
         <div className="row">
           <div className="col col--4">
-            <img src="img/screenshot-1.png"></img>
+            <div className={styles.screenshot}>
+              <img src="img/screenshot-1.png"></img>
+            </div>
           </div>
           <div className="col col--4">
-            <img src="img/screenshot-2.png"></img>
+            <div className={styles.screenshot}>
+              <img src="img/screenshot-2.png"></img>
+            </div>
           </div>
           <div className="col col--4">
-            <img src="img/screenshot-3.png"></img>
+            <div className={styles.screenshot}>
+              <img src="img/screenshot-3.png"></img>
+            </div>
           </div>
         </div>
         <div className="row">
           <div className="col col--4">
-            <img src="img/screenshot-4.png"></img>
+            <div className={styles.screenshot}>
+              <img src="img/screenshot-4.png"></img>
+            </div>
           </div>
           <div className="col col--4">
-            <img src="img/screenshot-5.png"></img>
+            <div className={styles.screenshot}>
+              <img src="img/screenshot-5.png"></img>
+            </div>
           </div>
           <div className="col col--4">
-            <img src="img/screenshot-6.png"></img>
+            <div className={styles.screenshot}>
+              <img src="img/screenshot-6.png"></img>
+            </div>
           </div>
         </div>
       </div>
