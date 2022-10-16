e57a79932ad2ab295bfb15d7506596619034226b
MM20 <15646950+MM2-0@users.noreply.github.com>
2022-10-16 12:00:58 +0200

    Update AGP

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:7.3.0")
+        classpath("com.android.tools.build:gradle:7.3.1")
         classpath(libs.kotlin.gradle)
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.10")
         // NOTE: Do not place your application dependencies here; they belong
