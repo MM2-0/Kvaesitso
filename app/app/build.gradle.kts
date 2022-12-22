fbf85ce9091104388ee08400dca0055ee39547f8
MM20 <15646950+MM2-0@users.noreply.github.com>
2022-12-22 14:50:27 +0100

    Version 1.20.0

--- app/app/build.gradle.kts
+++ app/app/build.gradle.kts
@@ -26,7 +26,7 @@ android {
         minSdk = sdk.versions.minSdk.get().toInt()
         targetSdk = sdk.versions.targetSdk.get().toInt()
         versionCode = versionCodeDate()
-        versionName = "1.19.1"
+        versionName = "1.20.0"
         signingConfig = signingConfigs.getByName("debug")
     }
     buildTypes {
