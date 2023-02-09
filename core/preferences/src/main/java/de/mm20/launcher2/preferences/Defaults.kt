fc2e239dfacbfb562cebbf83ba59496f2802804f
MM20 <15646950+MM2-0@users.noreply.github.com>
2023-02-09 14:06:11 +0100

    Don't enable favorites part by default

--- core/preferences/src/main/java/de/mm20/launcher2/preferences/Defaults.kt
+++ core/preferences/src/main/java/de/mm20/launcher2/preferences/Defaults.kt
@@ -51,7 +51,7 @@ fun createFactorySettings(context: Context): Settings {
                 .setAlarmPart(true)
                 .setBatteryPart(true)
                 .setMusicPart(true)
-                .setFavoritesPart(true)
+                .setFavoritesPart(false)
                 .setFillHeight(true)
                 .build()
         )
