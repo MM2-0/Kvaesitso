e1c23f3cbcab8c75aecd19e399790c27ab90dbe0
Sir-Photch <christoph.liebender@posteo.de>
2023-03-02 18:17:29 +0100

    weight factors

--- data/favorites/src/main/java/de/mm20/launcher2/favorites/FavoritesRepository.kt
+++ data/favorites/src/main/java/de/mm20/launcher2/favorites/FavoritesRepository.kt
@@ -217,8 +217,8 @@ internal class FavoritesRepositoryImpl(
                 val weightFactor =
                     when (dataStore.data.map { it.resultOrdering.weightFactor }.firstOrNull()) {
                         WeightFactor.Low -> 0.1
-                        WeightFactor.High -> 0.9
-                        else -> 0.5
+                        WeightFactor.High -> 0.3
+                        else -> 0.2
                     }
                 val item = SavedSearchable(searchable.key, searchable, 0, 0, false, 0.0)
                 item.toDatabaseEntity()?.let {
