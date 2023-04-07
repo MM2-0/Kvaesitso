20543861792b1aa2e2f7285407da3f34e229b010
MM20 <15646950+MM2-0@users.noreply.github.com>
2023-04-07 21:49:36 +0200

    Settings main screen: remove nested preference category

--- app/ui/src/main/java/de/mm20/launcher2/ui/settings/main/MainSettingsScreen.kt
+++ app/ui/src/main/java/de/mm20/launcher2/ui/settings/main/MainSettingsScreen.kt
@@ -1,10 +1,15 @@
 package de.mm20.launcher2.ui.settings.main
 
-import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.rounded.*
+import androidx.compose.material.icons.rounded.AccountBox
+import androidx.compose.material.icons.rounded.BugReport
+import androidx.compose.material.icons.rounded.Gesture
+import androidx.compose.material.icons.rounded.Info
+import androidx.compose.material.icons.rounded.Palette
+import androidx.compose.material.icons.rounded.Search
+import androidx.compose.material.icons.rounded.SettingsBackupRestore
+import androidx.compose.material.icons.rounded.Widgets
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import de.mm20.launcher2.ui.R
 import de.mm20.launcher2.ui.component.preferences.Preference
@@ -21,80 +26,78 @@ fun MainSettingsScreen() {
     ) {
         item {
             PreferenceCategory {
-                PreferenceCategory {
-                    Preference(
-                        icon = Icons.Rounded.Palette,
-                        title = stringResource(id = R.string.preference_screen_appearance),
-                        summary = stringResource(id = R.string.preference_screen_appearance_summary),
-                        onClick = {
-                            navController?.navigate("settings/appearance")
-                        }
-                    )
-                    Preference(
-                        icon = Icons.Rounded.Search,
-                        title = stringResource(id = R.string.preference_screen_search),
-                        summary = stringResource(id = R.string.preference_screen_search_summary),
-                        onClick = {
-                            navController?.navigate("settings/search")
-                        }
-                    )
-                    Preference(
-                        icon = Icons.Rounded.Widgets,
-                        title = stringResource(id = R.string.preference_screen_widgets),
-                        summary = stringResource(id = R.string.preference_screen_widgets_summary),
-                        onClick = {
-                            navController?.navigate("settings/widgets")
-                        }
-                    )
-                    Preference(
-                        icon = Icons.Rounded.Gesture,
-                        title = stringResource(id = R.string.preference_screen_gestures),
-                        summary = stringResource(id = R.string.preference_screen_gestures_summary),
-                        onClick = {
-                            navController?.navigate("settings/gestures")
-                        }
-                    )
-                    Preference(
-                        icon = Icons.Rounded.NotificationBadge,
-                        title = stringResource(id = R.string.preference_screen_badges),
-                        summary = stringResource(id = R.string.preference_screen_badges_summary),
-                        onClick = {
-                            navController?.navigate("settings/badges")
-                        }
-                    )
-                    Preference(
-                        icon = Icons.Rounded.AccountBox,
-                        title = stringResource(id = R.string.preference_screen_services),
-                        summary = stringResource(id = R.string.preference_screen_services_summary),
-                        onClick = {
-                            navController?.navigate("settings/accounts")
-                        }
-                    )
-                    Preference(
-                        icon = Icons.Rounded.SettingsBackupRestore,
-                        title = stringResource(id = R.string.preference_screen_backup),
-                        summary = stringResource(id = R.string.preference_screen_backup_summary),
-                        onClick = {
-                            navController?.navigate("settings/backup")
-                        }
-                    )
-                    Preference(
-                        icon = Icons.Rounded.BugReport,
-                        title = stringResource(id = R.string.preference_screen_debug),
-                        summary = stringResource(id = R.string.preference_screen_debug_summary),
-                        onClick = {
-                            navController?.navigate("settings/debug")
-                        }
-                    )
-                    Preference(
-                        icon = Icons.Rounded.Info,
-                        title = stringResource(id = R.string.preference_screen_about),
-                        summary = stringResource(id = R.string.preference_screen_about_summary),
-                        onClick = {
-                            navController?.navigate("settings/about")
-                        }
-                    )
-                }
+                Preference(
+                    icon = Icons.Rounded.Palette,
+                    title = stringResource(id = R.string.preference_screen_appearance),
+                    summary = stringResource(id = R.string.preference_screen_appearance_summary),
+                    onClick = {
+                        navController?.navigate("settings/appearance")
+                    }
+                )
+                Preference(
+                    icon = Icons.Rounded.Search,
+                    title = stringResource(id = R.string.preference_screen_search),
+                    summary = stringResource(id = R.string.preference_screen_search_summary),
+                    onClick = {
+                        navController?.navigate("settings/search")
+                    }
+                )
+                Preference(
+                    icon = Icons.Rounded.Widgets,
+                    title = stringResource(id = R.string.preference_screen_widgets),
+                    summary = stringResource(id = R.string.preference_screen_widgets_summary),
+                    onClick = {
+                        navController?.navigate("settings/widgets")
+                    }
+                )
+                Preference(
+                    icon = Icons.Rounded.Gesture,
+                    title = stringResource(id = R.string.preference_screen_gestures),
+                    summary = stringResource(id = R.string.preference_screen_gestures_summary),
+                    onClick = {
+                        navController?.navigate("settings/gestures")
+                    }
+                )
+                Preference(
+                    icon = Icons.Rounded.NotificationBadge,
+                    title = stringResource(id = R.string.preference_screen_badges),
+                    summary = stringResource(id = R.string.preference_screen_badges_summary),
+                    onClick = {
+                        navController?.navigate("settings/badges")
+                    }
+                )
+                Preference(
+                    icon = Icons.Rounded.AccountBox,
+                    title = stringResource(id = R.string.preference_screen_services),
+                    summary = stringResource(id = R.string.preference_screen_services_summary),
+                    onClick = {
+                        navController?.navigate("settings/accounts")
+                    }
+                )
+                Preference(
+                    icon = Icons.Rounded.SettingsBackupRestore,
+                    title = stringResource(id = R.string.preference_screen_backup),
+                    summary = stringResource(id = R.string.preference_screen_backup_summary),
+                    onClick = {
+                        navController?.navigate("settings/backup")
+                    }
+                )
+                Preference(
+                    icon = Icons.Rounded.BugReport,
+                    title = stringResource(id = R.string.preference_screen_debug),
+                    summary = stringResource(id = R.string.preference_screen_debug_summary),
+                    onClick = {
+                        navController?.navigate("settings/debug")
+                    }
+                )
+                Preference(
+                    icon = Icons.Rounded.Info,
+                    title = stringResource(id = R.string.preference_screen_about),
+                    summary = stringResource(id = R.string.preference_screen_about_summary),
+                    onClick = {
+                        navController?.navigate("settings/about")
+                    }
+                )
             }
         }
     }
