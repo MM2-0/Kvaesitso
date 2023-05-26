65d15a950b463bd64b228a1db56c80b6202822a1
MM20 <15646950+MM2-0@users.noreply.github.com>
2023-05-26 14:21:38 +0200

    Update Jetpack Compose

--- app/ui/src/main/java/de/mm20/launcher2/ui/launcher/widgets/clock/ClockWidget.kt
+++ app/ui/src/main/java/de/mm20/launcher2/ui/launcher/widgets/clock/ClockWidget.kt
@@ -12,6 +12,7 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.material3.LocalContentColor
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
@@ -108,7 +109,7 @@ fun ClockWidget(
                 ) {
                     if (partProviders.size > 1) {
                         HorizontalPager(
-                            pageCount = 2,
+                            state =  rememberPagerState { 2 },
                             beyondBoundsPageCount = 1,
                             modifier = Modifier.weight(1f)
                         ) {
