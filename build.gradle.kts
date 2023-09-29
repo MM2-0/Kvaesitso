803d35cda96c86e4a73f3a9b6a9840dd6708d181
MM20 <15646950+MM2-0@users.noreply.github.com>
2023-09-29 19:22:48 +0200

    Update AGP

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,7 +12,7 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:8.1.0")
+        classpath("com.android.tools.build:gradle:8.1.2")
         classpath(libs.kotlin.gradle)
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
