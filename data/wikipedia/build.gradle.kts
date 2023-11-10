c7f1ee7e9b7be0b133d5b80468afa64effa196df
Sir-Photch <christoph.liebender@posteo.de>
2023-11-10 14:46:25 +0100

    remove wikipedia minification setting

--- data/wikipedia/build.gradle.kts
+++ data/wikipedia/build.gradle.kts
@@ -15,7 +15,6 @@ android {
 
     buildTypes {
         release {
-            isMinifyEnabled = false
             proguardFiles(
                     getDefaultProguardFile("proguard-android-optimize.txt"),
                     "proguard-rules.pro"
