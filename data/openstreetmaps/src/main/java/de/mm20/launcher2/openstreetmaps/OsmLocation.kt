9dc9fb46afb32631aa0357080cf5f6e9bc8eca28
Sir-Photch <christoph.liebender@posteo.de>
2023-11-10 15:00:17 +0100

    move osm-specific opening-time parsing to OsmLocation

--- data/openstreetmaps/src/main/java/de/mm20/launcher2/openstreetmaps/OsmLocation.kt
+++ data/openstreetmaps/src/main/java/de/mm20/launcher2/openstreetmaps/OsmLocation.kt
@@ -4,11 +4,18 @@ import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.net.Uri
+import android.util.Log
 import de.mm20.launcher2.ktx.tryStartActivity
 import de.mm20.launcher2.search.Location
 import de.mm20.launcher2.search.LocationCategory
 import de.mm20.launcher2.search.OpeningTime
 import de.mm20.launcher2.search.SearchableSerializer
+import java.time.DayOfWeek
+import java.time.Duration
+import java.time.LocalTime
+import java.time.format.DateTimeFormatter
+import java.time.format.DateTimeParseException
+import java.time.format.ResolverStyle
 import java.util.Locale
 
 internal data class OsmLocation(
@@ -67,7 +74,7 @@ internal data class OsmLocation(
                 longitude = it.lon,
                 street = it.tags["addr:street"],
                 houseNumber = it.tags["addr:housenumber"],
-                openingHours = it.tags["opening_hours"]?.let { OpeningTime.fromOverpassElement(it) },
+                openingHours = it.tags["opening_hours"]?.let { ot -> parseOpeningTimes(ot) },
                 websiteUrl = it.tags["website"],
                 preferDetailsOverLaunch = !it.tags.containsKey("website"),
             )
@@ -75,4 +82,159 @@ internal data class OsmLocation(
 
         const val DOMAIN = "OpenStreetMaps"
     }
+}
+
+// allow for 24:00 to be part of the same day
+// https://stackoverflow.com/a/31113244
+private val DATE_TIME_FORMATTER =
+    DateTimeFormatter.ISO_LOCAL_TIME.withResolverStyle(ResolverStyle.SMART)
+
+private val timeRegex by lazy {
+    Regex(
+        """^(?:\d{2}:\d{2}-?){2}$""",
+        RegexOption.IGNORE_CASE
+    )
+}
+private val singleDayRegex by lazy {
+    Regex(
+        """^[mtwfsp][ouehra]$""",
+        RegexOption.IGNORE_CASE
+    )
+}
+private val dayRangeRegex by lazy {
+    Regex(
+        """^[mtwfsp][ouehra]-[mtwfsp][ouehra]$""",
+        RegexOption.IGNORE_CASE
+    )
+}
+
+internal fun parseOpeningTimes(it: String?): List<OpeningTime>? {
+    if (it.isNullOrBlank()) return null
+
+    val openingTimes = mutableListOf<OpeningTime>()
+
+    // e.g.
+    // "Mo-Sa 11:00-14:00, 17:00-23:00; Su 11:00-23:00"
+    // "Mo-Sa 11:00-21:00; PH,Su off"
+    // "Mo-Th 10:00-24:00, Fr,Sa 10:00-05:00, PH,Su 12:00-22:00"
+    var blocks =
+        it.split(',', ';', ' ').mapNotNull { if (it.isBlank()) null else it.trim() }
+
+    fun dayOfWeekFromString(it: String): DayOfWeek? = when (it.lowercase()) {
+        "mo" -> DayOfWeek.MONDAY
+        "tu" -> DayOfWeek.TUESDAY
+        "we" -> DayOfWeek.WEDNESDAY
+        "th" -> DayOfWeek.THURSDAY
+        "fr" -> DayOfWeek.FRIDAY
+        "sa" -> DayOfWeek.SATURDAY
+        "su" -> DayOfWeek.SUNDAY
+        else -> null
+    }
+
+    fun parseGroup(group: List<String>) {
+        if (group.isEmpty())
+            return
+
+        var times = group
+            .filter { timeRegex.matches(it) }
+            .mapNotNull {
+                try {
+                    val startTime =
+                        LocalTime.parse(it.substringBefore('-'), DATE_TIME_FORMATTER)
+                    val endTime =
+                        LocalTime.parse(it.substringAfter('-'), DATE_TIME_FORMATTER)
+
+                    var duration = Duration.between(startTime, endTime)
+
+                    if (duration.isNegative || duration.isZero)
+                        duration += Duration.ofDays(1)
+
+                    startTime to duration
+                } catch (dtpe: DateTimeParseException) {
+                    Log.e(
+                        "OpeningTimeFromOverpassElement",
+                        "Failed to parse opening time $it",
+                        dtpe
+                    )
+                    null
+                }
+            }
+
+        var days = group
+            .filter { dayRangeRegex.matches(it) }
+            .flatMap {
+                val dowStart = dayOfWeekFromString(it.substringBefore('-'))
+                    ?: return@flatMap emptyList()
+                val dowEnd = dayOfWeekFromString(it.substringAfter('-'))
+                    ?: return@flatMap emptyList()
+
+                val daysOfWeek = enumValues<DayOfWeek>().toList()
+
+                if (dowStart.ordinal <= dowEnd.ordinal)
+                    daysOfWeek.subList(dowStart.ordinal, dowEnd.ordinal + 1)
+                else // "We-Mo"
+                    daysOfWeek.subList(dowStart.ordinal, daysOfWeek.size)
+                        .union(daysOfWeek.subList(0, dowEnd.ordinal + 1))
+            }.union(
+                group.filter { singleDayRegex.matches(it) }
+                    .mapNotNull { dayOfWeekFromString(it) }
+            )
+
+        // if no time specified, treat as "all day" TODO really?
+        if (times.isEmpty())
+            times = listOf(LocalTime.MIDNIGHT to Duration.ofDays(1))
+
+        // if no day specified, treat as "every day"
+        if (days.isEmpty())
+            days = enumValues<DayOfWeek>().toSet()
+
+        openingTimes.addAll(days.flatMap { day ->
+            times.map { (start, duration) ->
+                OpeningTime(
+                    dayOfWeek = day,
+                    startTime = start,
+                    duration = duration
+                )
+            }
+        })
+    }
+
+    while (true) {
+        if (blocks.isEmpty())
+            break
+
+        // assuming that there are blocks that only contain time
+        // treating them as "every day of the week"
+        if (blocks.size < 2) {
+            parseGroup(blocks)
+            break
+        }
+
+        val nextTimeIndex =
+            blocks.indexOfFirst { timeRegex.matches(it) }
+
+        // no time left, so probably no sensible information
+        // willingly skips "off" and "closed" as they are not useful
+        if (nextTimeIndex == -1)
+            break
+
+        // assuming next block to start with the first date coming after a time block
+        var nextGroupIndex =
+            blocks.subList(nextTimeIndex, blocks.size)
+                .indexOfFirst { !timeRegex.matches(it) }
+
+        // no day left, so we are done
+        if (nextGroupIndex == -1) {
+            parseGroup(blocks)
+            break
+        }
+
+        // convert index from sublist context
+        nextGroupIndex += nextTimeIndex
+
+        parseGroup(blocks.subList(0, nextGroupIndex))
+        blocks = blocks.subList(nextGroupIndex, blocks.size)
+    }
+
+    return openingTimes
 }
