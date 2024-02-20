22b1e14c1158ede4019c24ba0d0723aa932ac215
MM20 <15646950+MM2-0@users.noreply.github.com>
2024-02-20 22:17:36 +0100

    Version 1.29.0

--- app/app/build.gradle.kts
+++ app/app/build.gradle.kts
@@ -31,8 +31,8 @@ android {
         minSdk = libs.versions.minSdk.get().toInt()
         targetSdk = libs.versions.targetSdk.get().toInt()
         @SuppressLint("HighAppVersionCode")
-        versionCode = System.getenv("VERSION_CODE_OVERRIDE")?.toIntOrNull() ?: 2023101300
-        versionName = "1.28.0"
+        versionCode = System.getenv("VERSION_CODE_OVERRIDE")?.toIntOrNull() ?: 2024022000
+        versionName = "1.29.0"
         signingConfig = signingConfigs.getByName("debug")
     }
 
