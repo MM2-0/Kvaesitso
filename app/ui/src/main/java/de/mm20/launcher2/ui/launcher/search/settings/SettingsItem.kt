c4fd4fbc1914e438d30c8690babf3988b56daa4f
Guillermo Villafuerte <gvillafu@comunidad.unam.mx>
2024-04-08 18:19:56 -0600

    Small refactor, including current icon.
    Signed-off-by: Guillermo Villafuerte <gvillafu@comunidad.unam.mx>

--- app/ui/src/main/java/de/mm20/launcher2/ui/launcher/search/settings/SettingsItem.kt
+++ app/ui/src/main/java/de/mm20/launcher2/ui/launcher/search/settings/SettingsItem.kt
@@ -1,27 +1,49 @@
 package de.mm20.launcher2.ui.launcher.search.settings
 
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import de.mm20.launcher2.search.data.PojoSettings
 import de.mm20.launcher2.ui.R
+import de.mm20.launcher2.ui.component.ShapedLauncherIcon
+import de.mm20.launcher2.ui.launcher.search.common.SearchableItemVM
+import de.mm20.launcher2.ui.launcher.search.listItemViewModel
 
 @Composable
 fun SettingsItem(
     data: PojoSettings,
     modifier: Modifier = Modifier)
 {
+    val viewModel: SearchableItemVM = listItemViewModel(key = "search-${data.key}")
+    val icon by viewModel.icon.collectAsStateWithLifecycle()
+
     Row(
         modifier = modifier.padding(8.dp)
     ) {
-        Text(
-            text = stringResource(R.string.settings_placeholder, data.label),
-            style = MaterialTheme.typography.titleMedium
-        )
+        Column {
+            ShapedLauncherIcon(
+                size = 48.dp,
+                icon = { icon },
+            )
+        }
+        Box(modifier = Modifier.width(8.dp))
+        Column(
+            modifier = Modifier.align(Alignment.CenterVertically)
+        ) {
+            Text(
+                text = stringResource(R.string.settings_placeholder, data.label),
+                style = MaterialTheme.typography.titleMedium,
+                overflow = TextOverflow.Ellipsis
+            )
+        }
+
     }
 }
