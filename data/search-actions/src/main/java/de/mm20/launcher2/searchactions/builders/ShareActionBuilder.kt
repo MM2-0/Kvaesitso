457d950006f8cdbe5812abe4bde5c6da9a011cc7
MM20 <15646950+MM2-0@users.noreply.github.com>
2024-04-19 23:08:10 +0200

    Add share search action

---
+++ data/search-actions/src/main/java/de/mm20/launcher2/searchactions/builders/ShareActionBuilder.kt
@@ -0,0 +1,27 @@
+package de.mm20.launcher2.searchactions.builders
+
+import android.content.Context
+import de.mm20.launcher2.searchactions.R
+import de.mm20.launcher2.searchactions.TextClassificationResult
+import de.mm20.launcher2.searchactions.actions.SearchAction
+import de.mm20.launcher2.searchactions.actions.SearchActionIcon
+import de.mm20.launcher2.searchactions.actions.ShareAction
+
+class ShareActionBuilder(
+    override val label: String,
+) : SearchActionBuilder {
+
+    constructor(context: Context) : this(context.getString(R.string.search_action_share))
+
+    override val key: String
+        get() = "share"
+
+    override val icon: SearchActionIcon = SearchActionIcon.Share
+
+    override fun build(context: Context, classifiedQuery: TextClassificationResult): SearchAction? {
+        return ShareAction(
+            context.getString(R.string.search_action_share),
+            classifiedQuery.text
+        )
+    }
+}
