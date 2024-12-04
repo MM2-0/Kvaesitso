37db43405fd0535195d94a702bf65e838f7fc3df
MM20 <15646950+MM2-0@users.noreply.github.com>
2024-12-04 18:56:36 +0100

    Catch potential SecurityException in app deserializer
    Close #1146

--- data/applications/src/main/java/de/mm20/launcher2/applications/AppSerialization.kt
+++ data/applications/src/main/java/de/mm20/launcher2/applications/AppSerialization.kt
@@ -43,33 +43,36 @@ class LauncherAppSerializer : SearchableSerializer {
 
 class LauncherAppDeserializer(val context: Context) : SearchableDeserializer {
     override suspend fun deserialize(serialized: String): SavableSearchable? {
-        val json = JSONObject(serialized)
-        val launcherApps = context.getSystemService<LauncherApps>()!!
-        val userManager = context.getSystemService<UserManager>()!!
-        val userSerial = json.optLong("user")
-        val user = userManager.getUserForSerialNumber(userSerial) ?: return null
-
-        val pkg = json.getString("package")
-        val activity = json.getString("activity")
-
-        val componentName = ComponentName(pkg, activity)
-
-        if (isAtLeastApiLevel(35)) {
-            val launcherUser = launcherApps.getLauncherUserInfo(user) ?: return null
-            if (launcherUser.userType == UserManager.USER_TYPE_PROFILE_PRIVATE && userManager.isQuietModeEnabled(user)) {
-                return LockedPrivateProfileApp(
-                    label = context.getString(R.string.app_label_locked_profile),
-                    componentName = componentName,
-                    user = user,
-                    userSerialNumber = userSerial
-                )
-            }
-        }
-
-        val intent = Intent().also {
-            it.component = componentName
-        }
         try {
+            val json = JSONObject(serialized)
+            val launcherApps = context.getSystemService<LauncherApps>()!!
+            val userManager = context.getSystemService<UserManager>()!!
+            val userSerial = json.optLong("user")
+            val user = userManager.getUserForSerialNumber(userSerial) ?: return null
+
+            val pkg = json.getString("package")
+            val activity = json.getString("activity")
+
+            val componentName = ComponentName(pkg, activity)
+
+            if (isAtLeastApiLevel(35)) {
+                val launcherUser = launcherApps.getLauncherUserInfo(user) ?: return null
+                if (launcherUser.userType == UserManager.USER_TYPE_PROFILE_PRIVATE && userManager.isQuietModeEnabled(
+                        user
+                    )
+                ) {
+                    return LockedPrivateProfileApp(
+                        label = context.getString(R.string.app_label_locked_profile),
+                        componentName = componentName,
+                        user = user,
+                        userSerialNumber = userSerial
+                    )
+                }
+            }
+
+            val intent = Intent().also {
+                it.component = componentName
+            }
             val launcherActivityInfo = launcherApps.resolveActivity(intent, user) ?: return null
             return LauncherApp(context, launcherActivityInfo)
         } catch (e: SecurityException) {
