3807549494ea618368fc720babd231c551952a06
thunder04 <70254851+thunder04@users.noreply.github.com>
2025-01-23 21:42:53 +0200

    Fix crash after tapping "Configure widget" in Settings > Home screen > Clock
    Closes #1221

--- app/ui/src/main/java/de/mm20/launcher2/ui/launcher/widgets/clock/WatchFaceSelector.kt
+++ app/ui/src/main/java/de/mm20/launcher2/ui/launcher/widgets/clock/WatchFaceSelector.kt
@@ -4,6 +4,7 @@ import android.app.Activity
 import android.app.ActivityOptions
 import android.appwidget.AppWidgetManager
 import android.content.Context
+import android.content.ContextWrapper
 import android.os.Build
 import androidx.compose.animation.AnimatedContent
 import androidx.compose.animation.animateContentSize
@@ -210,7 +211,7 @@ fun WatchFaceSelector(
                                             },
                                             onClick = {
                                                 appWidgetHost.startAppWidgetConfigureActivityForResult(
-                                                    context as Activity,
+                                                    getActivityFromContext(context) ?: return@DropdownMenuItem,
                                                     selected.widgetId ?: return@DropdownMenuItem,
                                                     0,
                                                     0,
@@ -219,9 +220,6 @@ fun WatchFaceSelector(
                                                     } else {
                                                         ActivityOptions.makeBasic()
                                                             .setPendingIntentBackgroundActivityStartMode(
-                                                                ActivityOptions.MODE_BACKGROUND_ACTIVITY_START_ALLOWED
-                                                            )
-                                                            .setPendingIntentCreatorBackgroundActivityStartMode(
                                                                 ActivityOptions.MODE_BACKGROUND_ACTIVITY_START_ALLOWED
                                                             )
                                                             .toBundle()
@@ -588,4 +586,18 @@ private fun ResizeCustomWidget(
             }
         }
     }
+}
+
+private fun getActivityFromContext(context: Context): Activity? {
+    var activity = context
+
+    while (activity is ContextWrapper) {
+        if (activity is Activity) {
+            return activity
+        }
+
+        activity = activity.baseContext
+    }
+
+    return null
 }
