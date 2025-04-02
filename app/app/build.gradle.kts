811d3c8b289f65519f3c7d627dbe5f45650824d7
MM20 <15646950+MM2-0@users.noreply.github.com>
2025-04-02 23:30:24 +0200

    Version 1.35.0

--- app/app/build.gradle.kts
+++ app/app/build.gradle.kts
@@ -31,8 +31,8 @@ android {
         minSdk = libs.versions.minSdk.get().toInt()
         targetSdk = libs.versions.targetSdk.get().toInt()
         @SuppressLint("HighAppVersionCode")
-        versionCode = System.getenv("VERSION_CODE_OVERRIDE")?.toIntOrNull() ?: 2024122400
-        versionName = "1.34.1"
+        versionCode = System.getenv("VERSION_CODE_OVERRIDE")?.toIntOrNull() ?: 2025040200
+        versionName = "1.35.0"
         signingConfig = signingConfigs.getByName("debug")
     }
 
