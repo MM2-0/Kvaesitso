ff5551989e400bd0c5861ff00fb8f21b92785a1d
MM20 <15646950+MM2-0@users.noreply.github.com>
2025-12-05 13:07:01 +0100

    Add icons to switches

--- app/ui/src/main/java/de/mm20/launcher2/ui/component/preferences/PreferenceWithSwitch.kt
+++ app/ui/src/main/java/de/mm20/launcher2/ui/component/preferences/PreferenceWithSwitch.kt
@@ -3,15 +3,19 @@ package de.mm20.launcher2.ui.component.preferences
 import androidx.annotation.DrawableRes
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.material3.Icon
 import androidx.compose.material3.LocalContentColor
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Switch
+import androidx.compose.material3.SwitchDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import de.mm20.launcher2.ui.R
 
 @Composable
 fun PreferenceWithSwitch(
@@ -52,6 +56,13 @@ fun PreferenceWithSwitch(
             checked = switchValue,
             enabled = enabled,
             onCheckedChange = onSwitchChanged,
+            thumbContent = {
+                Icon(
+                    painterResource(if (switchValue) R.drawable.check_20px else R.drawable.close_20px),
+                    modifier = Modifier.size(SwitchDefaults.IconSize),
+                    contentDescription = null,
+                )
+            }
         )
     }
 }
--- app/ui/src/main/java/de/mm20/launcher2/ui/component/preferences/SwitchPreference.kt
+++ app/ui/src/main/java/de/mm20/launcher2/ui/component/preferences/SwitchPreference.kt
@@ -1,11 +1,16 @@
 package de.mm20.launcher2.ui.component.preferences
 
 import androidx.annotation.DrawableRes
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Switch
+import androidx.compose.material3.SwitchDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.painterResource
+import de.mm20.launcher2.ui.R
 
 @Composable
 fun SwitchPreference(
@@ -30,6 +35,13 @@ fun SwitchPreference(
         controls = {
             Switch(
                 enabled = enabled, checked = value, onCheckedChange = onValueChanged,
+                thumbContent = {
+                    Icon(
+                        painterResource(if (value) R.drawable.check_20px else R.drawable.close_20px),
+                        modifier = Modifier.size(SwitchDefaults.IconSize),
+                        contentDescription = null,
+                    )
+                }
             )
         },
         containerColor = containerColor,
