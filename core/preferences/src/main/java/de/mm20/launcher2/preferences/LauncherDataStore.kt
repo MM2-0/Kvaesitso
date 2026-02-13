1402cff360bf895c6679c23c5d42ecb1b132b067
Fidel Montesino <fidel.montesino@gmail.com>
2026-02-13 14:02:56 +0000

    Remove unnecessary Migration6

--- core/preferences/src/main/java/de/mm20/launcher2/preferences/LauncherDataStore.kt
+++ core/preferences/src/main/java/de/mm20/launcher2/preferences/LauncherDataStore.kt
@@ -6,7 +6,6 @@ import de.mm20.launcher2.preferences.migrations.Migration2
 import de.mm20.launcher2.preferences.migrations.Migration3
 import de.mm20.launcher2.preferences.migrations.Migration4
 import de.mm20.launcher2.preferences.migrations.Migration5
-import de.mm20.launcher2.preferences.migrations.Migration6
 import de.mm20.launcher2.settings.BaseSettings
 
 internal class LauncherDataStore(
@@ -20,7 +19,6 @@ internal class LauncherDataStore(
         Migration3(),
         Migration4(),
         Migration5(),
-        Migration6(),
     ),
     corruptionHandler = ReplaceFileCorruptionHandler { LauncherSettingsData() }
 ) {
