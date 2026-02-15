947f8cfbcd51bbcd2c306b45ad6c6859fd31a119
korigamik-hypr <korigamik@gmail.com>
2026-02-15 22:06:22 +0530

    Use notification gesture without accessibility dependency
    Open notifications via StatusBarManager reflection only, and remove the accessibility global-action fallback for notification drawer invocation.

--- services/global-actions/src/main/java/de/mm20/launcher2/globalactions/GlobalActionsService.kt
+++ services/global-actions/src/main/java/de/mm20/launcher2/globalactions/GlobalActionsService.kt
@@ -1,15 +1,14 @@
 package de.mm20.launcher2.globalactions
 
+import android.annotation.SuppressLint
 import android.accessibilityservice.AccessibilityService
 import android.content.Context
+import android.util.Log
+import java.lang.reflect.InvocationTargetException
 
 class GlobalActionsService(private val context: Context) {
     fun openNotificationDrawer() {
-        try {
-            expandNotificationPanel()
-        } catch (e: Exception) {
-            LauncherAccessibilityService.getInstance()?.performGlobalAction(AccessibilityService.GLOBAL_ACTION_NOTIFICATIONS)
-        }
+        expandNotificationPanel()
     }
 
     fun lockScreen() {
@@ -32,11 +31,22 @@ class GlobalActionsService(private val context: Context) {
         LauncherAccessibilityService.getInstance()?.performGlobalAction(AccessibilityService.GLOBAL_ACTION_RECENTS)
     }
 
+    @SuppressLint("WrongConstant")
     private fun expandNotificationPanel() {
-        val statusBarService = context.getSystemService("statusbar")
-        val statusBarManager = Class.forName("android.app.StatusBarManager")
-        val method = statusBarManager.getMethod("expandNotificationsPanel")
-        method.invoke(statusBarService)
+        try {
+            val statusBarService = context.getSystemService("statusbar") ?: return
+            val statusBarManager = Class.forName("android.app.StatusBarManager")
+            val method = statusBarManager.getMethod("expandNotificationsPanel")
+            method.invoke(statusBarService)
+        } catch (e: IllegalAccessException) {
+            Log.e("MM20", Log.getStackTraceString(e))
+        } catch (e: InvocationTargetException) {
+            Log.e("MM20", Log.getStackTraceString(e))
+        } catch (e: NoSuchMethodException) {
+            Log.e("MM20", Log.getStackTraceString(e))
+        } catch (e: ClassNotFoundException) {
+            Log.e("MM20", Log.getStackTraceString(e))
+        }
     }
 
     private fun expandQuickSettings() {
