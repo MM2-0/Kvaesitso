7e7f3f72aa704b89d0b0d83ad4d9898d834682b2
MM20 <15646950+MM2-0@users.noreply.github.com>
2026-05-31 18:15:11 +0200

    Version 1.40.2

--- app/app/build.gradle.kts
+++ app/app/build.gradle.kts
@@ -34,7 +34,7 @@ android {
         minSdk = libs.versions.minSdk.get().toInt()
         targetSdk = libs.versions.targetSdk.get().toInt()
         versionCode = System.getenv("VERSION_CODE_OVERRIDE")?.toIntOrNull() ?: 2026041400
-        versionName = "1.40.1"
+        versionName = "1.40.2"
         signingConfig = signingConfigs.getByName("debug")
     }
 
