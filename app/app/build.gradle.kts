328073f38f8472118a377f69cce34a2c86372ab9
MM20 <15646950+MM2-0@users.noreply.github.com>
2026-09-15 15:38:54 +0200

    Version 1.41.0

--- app/app/build.gradle.kts
+++ app/app/build.gradle.kts
@@ -33,8 +33,8 @@ android {
         applicationId = "de.mm20.launcher2"
         minSdk = libs.versions.minSdk.get().toInt()
         targetSdk = libs.versions.targetSdk.get().toInt()
-        versionCode = System.getenv("VERSION_CODE_OVERRIDE")?.toIntOrNull() ?: 2026053100
-        versionName = "1.40.2"
+        versionCode = System.getenv("VERSION_CODE_OVERRIDE")?.toIntOrNull() ?: 2026091500
+        versionName = "1.41.0"
         signingConfig = signingConfigs.getByName("debug")
     }
 
